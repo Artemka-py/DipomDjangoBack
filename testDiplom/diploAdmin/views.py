2d327faeed6638ec91dfa6ac9cdf0362b843d9cc
Artemka-py <djartlyt@gmail.com>
2021-03-10 15:29:52 +0300

    Fix views endpoints

--- testDiplom/diploAdmin/views.py
+++ testDiplom/diploAdmin/views.py
@@ -17,17 +17,30 @@ def dict_fetch_all(cursor):
 
 import json
 
+from rest_framework import serializers as ser
+from rest_framework.renderers import JSONRenderer
+
+class CommentSerializer(ser.Serializer):
+    email = ser.EmailField()
+    content = ser.CharField(max_length=200)
+    created = ser.DateTimeField()
+
 def projects(req, username):
+    #('project_id', 'project_name', 'project_client_login', 'project_manager_login', 'project_workgroup', 
+    # 'project_info', 'project_status', 'start_date_plan', 'finish_date_plan', 'start_date_fact', 'finish_date_fact', )
     with connection.cursor() as cursor:
-        cursor.execute(''' select project_id, project_name from projects inner join workgroups on
+        cursor.execute(''' select project_id, project_name, project_info, start_date_plan, finish_date_plan,
+        start_date_fact, finish_date_fact, status.status_name, workgroups.workgroup_name 
+        from projects inner join workgroups on
         projects.project_workgroup_id = workgroups.workgroup_id
         left join working_developer_list on workgroups.workgroup_id = working_developer_list.workgroup_id
         inner join clients on projects.project_client_login = clients.client_login
         inner JOIN managers on projects.project_manager_login = managers.manager_login
+        inner JOIN status on projects.project_status_id = status.status_id
         where client_login = %s or manager_login = %s or developer_login = %s  ''', [username, username, username])
 
         data = dict_fetch_all(cursor)
-        data = json.dumps(data)
+        data = JSONRenderer().render(data)
 
     return HttpResponse(data, content_type="application/json")
 
