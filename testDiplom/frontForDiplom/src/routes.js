6353016e43b61fd0a9cbfd17d47954f5997487d9
Artemka-py <djartlyt@gmail.com>
2021-04-08 11:25:54 +0300

    Fix routes

--- testDiplom/frontForDiplom/src/routes.js
+++ testDiplom/frontForDiplom/src/routes.js
@@ -6,20 +6,31 @@ import Auth from './components/Autentification/Auth/Auth';
 import Register from './components/Autentification/Registration/Register';
 import Project from './pages/Projects/Project';
 import Task from './pages/Tasks/Task';
+import { connect } from 'react-redux';
 
 const customHistory = createBrowserHistory();
 
-const BaseRouter = () => (
+const BaseRouter = ({ username, isAuthenticated }) => (
   <div>
     <Route path="/" />
     <Route path="/about" component={AboutPage} />
     <Route path="/admin" exact render={() => <Redirect to="/admin" />} />
     <Route path="/auth" component={Auth} />
     <Route path="/register" component={Register} />
-    <Route path="/projects" component={Project} />
-    <Route path="/tasks" component={Task} />
-    <Route path="/projects/:id" exact component="" />
+    {isAuthenticated === true && (
+      <>
+        <Route path="/projects" component={Project} />
+        <Route path="/tasks" component={Task} />
+        <Route path="/projects/:id" exact component="" />
+      </>
+    )}
   </div>
 );
+
+const mapStateToProps = (state) => {
+  return {
+    username: state.username,
+  };
+};
 
 export default BaseRouter;
