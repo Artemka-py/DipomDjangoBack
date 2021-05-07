e20f7846b958526408c5ba064f72028d89da3554
Artemka-py <djartlyt@gmail.com>
2021-05-07 09:14:11 +0300

    Add new features

--- testDiplom/frontForDiplom/src/components/Autentification/Auth/Auth.jsx
+++ testDiplom/frontForDiplom/src/components/Autentification/Auth/Auth.jsx
@@ -1,4 +1,4 @@
-import { Form, Input, Button, Checkbox } from 'antd';
+import { Form, Input, Button, Checkbox, Statistic, Space } from 'antd';
 import { UserOutlined, LockOutlined, EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
 import './Auth.css';
 import { connect } from 'react-redux';
@@ -6,17 +6,20 @@ import * as actions from '../../../store/actions/auth';
 import { NavLink, useHistory } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
 import ConfirmEmail from '../ConfirmEmail/ConfirmEmail';
+import moment from 'moment';
 
-// const antIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />;
+const { Countdown } = Statistic;
 
 const Auth = (props) => {
   const history = useHistory();
   const [confirmEmail, setConfirmEmail] = useState(false);
   const [username, setUsername] = useState(null);
   const [password, setPassword] = useState(null);
-  let errorMessage = null;
+  const [tryAuth, setTryAuth] = useState(0);
+  let errorMessage = [];
 
   const onFinish = async (values) => {
+    setTryAuth(tryAuth + 1);
     setUsername(values.username);
     setPassword(values.password);
     authLogic(values.username, values.password);
@@ -31,26 +34,33 @@ const Auth = (props) => {
       if (props.error === null) history.push('/');
   }, [props.error, props.history, props.loading, props.token]);
 
-  const onFinishFailed = (errorInfo) => {};
+  const onFinishFailed = (errorInfo) => {
+    setTryAuth(tryAuth + 1);
+  };
 
   useEffect(() => {
     if (props.error === 'Пройдите сначала активацию через почту!') {
       setConfirmEmail(true);
     }
-    if (props.error || errorMessage) {
-      errorMessage = (
-        <p style={{ color: 'red' }}>
-          {typeof props.error.response === 'undefined'
-            ? props.error
-            : props.error.response.data.non_field_errors}
-        </p>
-      );
-    }
   }, [props.error, errorMessage]);
+
+  if (props.error || errorMessage.length !== 0) {
+    if (typeof props.error.response === 'undefined') {
+      errorMessage.push(props.error);
+    } else {
+      for (let key in props.error.response.data) {
+        errorMessage.push(props.error.response.data[key]);
+      }
+    }
+  }
 
   return (
     <div style={{ marginLeft: '40%', height: '100vh', marginTop: '40px' }}>
-      {errorMessage}
+      {errorMessage.map((e, i) => (
+        <p style={{ color: 'red' }} key={i}>
+          {e}
+        </p>
+      ))}
       {confirmEmail ? (
         <ConfirmEmail
           username={username}
@@ -107,9 +117,9 @@ const Auth = (props) => {
               <Checkbox>Запомнить меня</Checkbox>
             </Form.Item>
 
-            <a className="login-form-forgot" style={{ float: 'right' }} href="">
-              Забыли пароль?
-            </a>
+            {/*<a className="login-form-forgot" style={{ float: 'right' }} href="">*/}
+            {/*  Забыли пароль?*/}
+            {/*</a>*/}
           </Form.Item>
 
           <Form.Item>
@@ -119,9 +129,22 @@ const Auth = (props) => {
               style={{ width: '100%' }}
               className="login-form-button"
               loading={props.loading}
+              disabled={tryAuth >= 3}
             >
               Войти
             </Button>
+            {tryAuth >= 3 ? (
+              <>
+                <hr />
+                <Space direction="horizontal" size={'small'}>
+                  <Countdown
+                    title={'Попробуйте еще раз через:'}
+                    value={Date.now() + 1000 * 60}
+                    onFinish={() => setTryAuth(2)}
+                  />
+                </Space>
+              </>
+            ) : null}
             <br />
             <br />
             Или <NavLink to="/register">зарегистрируйтесь сейчас!</NavLink>
